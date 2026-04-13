aec98b90fb455cb7693084511c90941cc90343a7
Leonardo <leonardolafa17@gmail.com>
2026-04-13 16:42:26 -0300

    teste

--- test.py
+++ test.py
@@ -3,4 +3,5 @@ print("This is a test file for the repository.")
 print("Testing complete.")
 def add(a, b):
     return a + b
-print("The sum of 5 and 3 is:", add(5, 3))
+print("The sum of 5 and 3 is:", add(5, 3))pr
+print("End of test file.")
